74c3e3e4eed25af0d3c6be1c3cbe5d78a4e3ff6b
chiplugmedia <chiplugtv@gmail.com>
2026-08-23 23:43:52 +0100

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Register from '@/pages/Register';
+import NotFound from "@/pages/NotFound";
 
 export default function App() {
   return (
@@ -8,6 +9,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        {/* Catch-all 404 Route - Must be placed last */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );

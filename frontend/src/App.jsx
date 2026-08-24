1ffdea0d58b87aab87ac1d21b80804a894c27ddd
chiplugmedia <chiplugtv@gmail.com>
2026-08-24 21:47:46 +0100

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import Register from '@/pages/Register';
 import NotFound from "@/pages/NotFound";
+import TelegramSupport from "@/components/TelegramSupport"; // Changed from @/pages/
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        {/* Catch-all 404 Route - Must be placed last */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <TelegramSupport />
+      </div>
     </BrowserRouter>
   );
 }
